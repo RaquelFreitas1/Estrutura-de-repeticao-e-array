921b0810db2a812f8e1695d62de58adbbdd7c09b
raquel <raqueel_@live.com>
2022-07-18 22:42:28 -0300

    Exercicios estrutura de repetição

---
+++ Notas.java
@@ -0,0 +1,26 @@
+package br.dio.loop;
+
+import java.util.Scanner;
+
+public class Notas {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10){
+
+            System.out.println("Nota invalida. Digite novamente: ");
+            nota = scan.nextInt();
+        }
+
+    }
+
+
+
+
+}
